4263c626376b3c99821ad50c7146fc9e506e2c65
Aleksey <68481433+Sir-Beard@users.noreply.github.com>
2021-10-27 18:28:31 +0300

    Delete Main.java

--- Main.java
+++
@@ -1,9 +0,0 @@
-package com.shpp.p2p.cs.ohubanov.assignment6;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    // no no, don't do anything here plz
-        // just look at subpackages!!
-    }
-}
